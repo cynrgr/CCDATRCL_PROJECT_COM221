b9835df4057bc043493ca5897992e1baf59785f1
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2023-11-13 00:45:51 +0800

    Update sticketsMainPage.java

--- DSAFinalProject/src/sticketsMainPage.java
+++ DSAFinalProject/src/sticketsMainPage.java
@@ -6,7 +6,6 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Panel;
@@ -81,12 +80,24 @@ public class sticketsMainPage {
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 
-		
 		JLabel lblEvents = new JLabel("EVENTS");
 		lblEvents.setFont(new Font("Tahoma", Font.BOLD, 30));
 		lblEvents.setForeground(new Color(255, 255, 255));
 		lblEvents.setBounds(20, 10, 150, 43);
 		panel.add(lblEvents);
+		
+		JButton lblSortEvent = new JButton("<html><u>SORT EVENTS BY DATE<html><u>");
+		lblSortEvent.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+   //             updateSticketsMainPage();
+            }
+        });
+		lblSortEvent.setForeground(new Color(203, 108, 230));
+		lblSortEvent.setBackground(new Color(255, 255, 255));
+		lblSortEvent.setFont(new Font("Tahoma", Font.BOLD, 10));
+        lblSortEvent.setBounds(10, 45, 153, 15);
+        panel.add(lblSortEvent);
 		
 		JLabel imgSvt = new JLabel("");
 		ImageIcon svt = new ImageIcon(this.getClass().getResource("/svt.jpg"));
@@ -97,7 +108,7 @@ public class sticketsMainPage {
 		JLabel lblSvtTitle = new JLabel("SEVENTEEN: Follow Tour");
 		lblSvtTitle.setForeground(Color.WHITE);
 		lblSvtTitle.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblSvtTitle.setBounds(134, 358, 213, 38);
+		lblSvtTitle.setBounds(134, 358, 215, 38);
 		panel.add(lblSvtTitle);
 		
 		JLabel lblSvtLoc = new JLabel("Philippine Sports Stadium");
@@ -105,6 +116,12 @@ public class sticketsMainPage {
 		lblSvtLoc.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblSvtLoc.setBounds(134, 384, 213, 27);
 		panel.add(lblSvtLoc);
+		
+		JLabel lblSvtDate = new JLabel("January 13, 2024");
+		lblSvtDate.setForeground(Color.WHITE);
+		lblSvtDate.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSvtDate.setBounds(134, 405, 213, 27);
+		panel.add(lblSvtDate);
 		
 		JButton btnSvtBuy = new JButton("BUY TICKETS");
 		btnSvtBuy.addActionListener(new ActionListener() {
@@ -116,7 +133,7 @@ public class sticketsMainPage {
 		btnSvtBuy.setFont(new Font("Tahoma", Font.BOLD, 20));
 		btnSvtBuy.setForeground(new Color(203, 108, 230));
 		btnSvtBuy.setBackground(new Color(255, 255, 255));
-		btnSvtBuy.setBounds(134, 417, 213, 43);
+		btnSvtBuy.setBounds(134, 440, 213, 43);
 		panel.add(btnSvtBuy);
 		
 		JLabel imgEnha = new JLabel("");
@@ -128,7 +145,7 @@ public class sticketsMainPage {
 		JLabel lblEnhaTitle = new JLabel("ENHYPEN: Manifesto");
 		lblEnhaTitle.setForeground(Color.WHITE);
 		lblEnhaTitle.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblEnhaTitle.setBounds(481, 358, 213, 38);
+		lblEnhaTitle.setBounds(481, 358, 215, 38);
 		panel.add(lblEnhaTitle);
 		
 		JLabel lblEnhaLoc = new JLabel("Mall of Asia Arena");
@@ -136,6 +153,12 @@ public class sticketsMainPage {
 		lblEnhaLoc.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblEnhaLoc.setBounds(481, 384, 213, 27);
 		panel.add(lblEnhaLoc);
+		
+		JLabel lblEnhaDate = new JLabel("February 4-5, 2023");
+		lblEnhaDate.setForeground(Color.WHITE);
+		lblEnhaDate.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblEnhaDate.setBounds(481, 405, 213, 27);
+		panel.add(lblEnhaDate);
 		
 		JButton btnEnhaBuy = new JButton("BUY TICKETS");
 		btnEnhaBuy.addActionListener(new ActionListener() {
@@ -147,7 +170,7 @@ public class sticketsMainPage {
 		btnEnhaBuy.setForeground(new Color(203, 108, 230));
 		btnEnhaBuy.setBackground(new Color(255, 255, 255));
 		btnEnhaBuy.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnEnhaBuy.setBounds(481, 417, 213, 43);
+		btnEnhaBuy.setBounds(481, 440, 213, 43);
 		panel.add(btnEnhaBuy);
 		
 		JLabel imgHarry = new JLabel("");
@@ -159,7 +182,7 @@ public class sticketsMainPage {
 		JLabel lblHarryTitle = new JLabel("Harry Styles: Love on Tour");
 		lblHarryTitle.setForeground(Color.WHITE);
 		lblHarryTitle.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblHarryTitle.setBounds(828, 358, 213, 38);
+		lblHarryTitle.setBounds(828, 358, 215, 38);
 		panel.add(lblHarryTitle);
 		
 		JLabel lblHarryLoc = new JLabel("Philippine Arena");
@@ -167,6 +190,12 @@ public class sticketsMainPage {
 		lblHarryLoc.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblHarryLoc.setBounds(828, 384, 213, 27);
 		panel.add(lblHarryLoc);
+		
+		JLabel lblHarryDate = new JLabel("March 14, 2023");
+		lblHarryDate.setForeground(Color.WHITE);
+		lblHarryDate.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblHarryDate.setBounds(828, 405, 213, 27);
+		panel.add(lblHarryDate);
 		
 		JButton btnHarryBuy = new JButton("BUY TICKETS");
 		btnHarryBuy.addActionListener(new ActionListener() {
@@ -178,7 +207,7 @@ public class sticketsMainPage {
 		btnHarryBuy.setForeground(new Color(203, 108, 230));
 		btnHarryBuy.setBackground(new Color(255, 255, 255));
 		btnHarryBuy.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnHarryBuy.setBounds(828, 417, 213, 43);
+		btnHarryBuy.setBounds(828, 440, 213, 43);
 		panel.add(btnHarryBuy);
 	}
 }
